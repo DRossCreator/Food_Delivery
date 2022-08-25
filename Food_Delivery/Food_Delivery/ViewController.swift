4276c1b2bbe259be0b1c86627789fb2b096fd553
YarkovenkoDev <99531855+YarkovenkoDev@users.noreply.github.com>
2022-08-25 17:52:39 +0500

    Add project

---
+++ Food_Delivery/Food_Delivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food_Delivery
+//
+//  Created by Daniil Yarkovenko on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+
+
+}
+
